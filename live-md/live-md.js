d18151a8589abea7315f205c03905fad27276f5e
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-11-08 12:27:28 +0530

    ading code to new values

---
+++ live-md/live-md.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const md = require("markdown-it") ();
+const chokidar = require ("chokidar");
+const browserSync = require("browser-sync").create();
+
+function render() {
+  const content = fs.readFileSync("README.md", "utf8");
+  const html = `
+    <html>
+      <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
+      <body>${md.render(content)}</body>
+    </html>`;
+  fs.writeFileSync("index.html", html);
+}
+
+render();
+browserSync.init({ server: ".", files: ["index.html"] });
+
+chokidar.watch("README.md").on("change", () => {
+  render();
+  browserSync.reload();
+});
