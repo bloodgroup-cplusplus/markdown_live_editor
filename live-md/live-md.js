82226dcf9d489733c4c8223dbf80225c68f5b02a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-11-10 09:09:17 +0530

    adding tls

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("UDP.md", "utf8");
+  const content = fs.readFileSync("TLS.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("UDP.md").on("change", () => {
+chokidar.watch("TLS.md").on("change", () => {
   render();
   browserSync.reload();
 });
