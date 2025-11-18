0c7ca1bc83b023274456d7bddc99a97691f1b5ac
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-11-18 09:15:49 +0530

    adding sql no sql

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("rate_limiting.md", "utf8");
+  const content = fs.readFileSync("sql-nosql.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("rate_limiting.md").on("change", () => {
+chokidar.watch("sql-nosql.md").on("change", () => {
   render();
   browserSync.reload();
 });
