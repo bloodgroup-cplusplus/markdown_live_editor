b8fef16af3d80297e7d184626d20ded09779fcf9
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-11-29 19:10:38 +0530

    adding acid

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("crud_database.md", "utf8");
+  const content = fs.readFileSync("acid.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("crud_database.md").on("change", () => {
+chokidar.watch("acid.md").on("change", () => {
   render();
   browserSync.reload();
 });
