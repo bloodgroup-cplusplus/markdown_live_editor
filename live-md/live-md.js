94a945cd4919b6e4f5bc476ca94e3d82504d3655
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-12-04 09:19:53 +0530

    adding software smoke

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("spof.md", "utf8");
+  const content = fs.readFileSync("software_testing_smoke.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("spof.md").on("change", () => {
+chokidar.watch("software_testing_smoke.md").on("change", () => {
   render();
   browserSync.reload();
 });
