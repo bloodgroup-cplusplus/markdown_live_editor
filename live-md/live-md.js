ceb87eb4e232c413d018b13cbc8d0f54a862a204
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-12-24 08:23:10 +0530

    adding client server architecture

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("microservices.md", "utf8");
+  const content = fs.readFileSync("client_server.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("microservices.md").on("change", () => {
+chokidar.watch("client_server.md").on("change", () => {
   render();
   browserSync.reload();
 });
