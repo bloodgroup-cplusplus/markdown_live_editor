4b0efd3da20ef75f1af4f91c1fabdf4f9803d8d1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-12-24 10:14:28 +0530

    adding stateufl vs stateless

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("client_server.md", "utf8");
+  const content = fs.readFileSync("stateful_vs_stateless.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("client_server.md").on("change", () => {
+chokidar.watch("stateful_vs_stateless.md").on("change", () => {
   render();
   browserSync.reload();
 });
