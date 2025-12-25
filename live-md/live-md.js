b5433108dab9c6d6a3088b0a80dfb2877e88881d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-12-25 11:13:47 +0530

    adding forward vs reverse proxy

--- live-md/live-md.js
+++ live-md/live-md.js
@@ -4,7 +4,7 @@ const chokidar = require("chokidar");
 const browserSync = require("browser-sync").create();
 
 function render() {
-  const content = fs.readFileSync("sync_async.md", "utf8");
+  const content = fs.readFileSync("forward_reverse_proxy.md", "utf8");
   const html = `
     <html>
       <head><meta charset="UTF-8"><title>Markdown Preview</title></head>
@@ -16,7 +16,7 @@ function render() {
 render();
 browserSync.init({ server: ".", files: ["index.html"] });
 
-chokidar.watch("sync_async.md").on("change", () => {
+chokidar.watch("forward_reverse_proxy.md").on("change", () => {
   render();
   browserSync.reload();
 });
